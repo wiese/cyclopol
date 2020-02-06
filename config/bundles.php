ac049b3beab99ab8df6d8dc584e2e190e3ab32c2
wiese <pablo@grass.digital>
2020-02-06 14:25:28 +0100

    allow CORS between nuxt frontend and symfony backend

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
 	Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => [ 'dev' => true ],
 	Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => [ 'all' => true ],
 	Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => [ 'dev' => true, 'test' => true ],
+	Nelmio\CorsBundle\NelmioCorsBundle::class => [ 'all' => true ],
 ];
